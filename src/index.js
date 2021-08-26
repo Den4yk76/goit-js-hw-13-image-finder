38c50f8afdaa241d2eb143ab4d7a54283c6c64b5
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-08-26 16:43:10 +0300

    pnotify added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import './sass/main.scss';
 import listMarkup from './templates/gallery-item-template.hbs';
 import { refs } from './js/refs';
 import { fetchApi } from './js/apiService';
+import { success, error, defaults } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+import '../node_modules/@pnotify/confirm/dist/PNotifyConfirm.css';
 
+defaults.delay = 1500;
 let pageNum = 1;
 
-// refs.input.addEventListener('keypress', makeMarkup);
 refs.form.addEventListener('submit', makeMarkup);
 refs.loadMoreBtn.addEventListener('click', makeMoreMarkup);
 
@@ -17,15 +20,16 @@ function makeMarkup(e) {
   fetchApi(refs.input.value, pageNum)
     .then(data => {
       if (data.hits.length === 0) {
-        console.log('Ничего не найдено!');
+        error({ text: `Ничего не найдено!` });
       } else {
         refs.gallery.insertAdjacentHTML('beforeend', listMarkup(data.hits));
         refs.loadMoreBtn.classList.remove('is-hidden');
+        success({ text: `Ваш ответ по запросу ${refs.input.value} :)` });
       }
     })
     .then(() => pageNum++)
     .catch(err => {
-      console.log('err', err);
+      error({ text: `Возникла ошибка ${err}. Попробуйте ещё` });
     });
 }
 
@@ -37,8 +41,11 @@ function makeMoreMarkup(e) {
       refs.gallery.insertAdjacentHTML('beforeend', listMarkup(data.hits));
     })
     .then(() => pageNum++)
-    .then(() => refs.gallery.scrollIntoView({ block: 'end', behavior: 'smooth' }))
+    .then(() => {
+      refs.gallery.scrollIntoView({ block: 'end', behavior: 'smooth' });
+      success({ text: `Добавлено больше ответов :)` });
+    })
     .catch(err => {
-      console.log('err', err);
+      error({ text: `Возникла ошибка ${err}. Попробуйте ещё` });
     });
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,7 +2,9 @@ const API_KEY = '23081749-b9f3d38fc4718cbd45a8beed9';
 const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
 export function fetchApi(searchQuery, pageNum) {
-  return fetch(`${BASE_URL}&q=${searchQuery}&page=${pageNum}&per_page=12&key=${API_KEY}`)
-    .then(response => response.json())
-    .catch(err => console.log('err', err));
+  if (searchQuery) {
+    return fetch(`${BASE_URL}&q=${searchQuery}&page=${pageNum}&per_page=12&key=${API_KEY}`)
+      .then(response => response.json())
+      .catch(err => error({ text: `Возникла ошибка ${err}. Попробуйте ещё` }));
+  }
 }
