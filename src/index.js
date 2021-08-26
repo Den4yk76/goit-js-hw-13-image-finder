d1ccfb1585e8328c983f36a18443950f45f0063f
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-08-26 14:11:00 +0300

    revision 1

--- src/index.js
+++ src/index.js
@@ -6,19 +6,38 @@ import { fetchApi } from './js/apiService';
 let pageNum = 1;
 
 refs.input.addEventListener('keypress', makeMarkup);
-refs.loadMoreBtn.addEventListener('click', makeMarkup);
+refs.loadMoreBtn.addEventListener('click', makeMoreMarkup);
 
 function makeMarkup(e) {
   if (e.keyCode === 13) {
     e.preventDefault();
+
     fetchApi(e.currentTarget.value, pageNum)
       .then(data => {
-        refs.gallery.insertAdjacentHTML('beforeend', listMarkup(data.hits));
-        refs.loadMoreBtn.classList.remove('is-hidden');
+        if (data.hits.length === 0) {
+          return;
+        } else {
+          refs.gallery.insertAdjacentHTML('beforeend', listMarkup(data.hits));
+          refs.loadMoreBtn.classList.remove('is-hidden');
+        }
       })
       .then(() => pageNum++)
       .catch(err => {
         console.log('err', err);
       });
   }
+}
+
+function makeMoreMarkup(e) {
+  e.preventDefault();
+
+  fetchApi(refs.input.value, pageNum)
+    .then(data => {
+      refs.gallery.insertAdjacentHTML('beforeend', listMarkup(data.hits));
+    })
+    .then(() => pageNum++)
+    .then(() => refs.gallery.scrollIntoView({ block: 'end', behavior: 'smooth' }))
+    .catch(err => {
+      console.log('err', err);
+    });
 }
